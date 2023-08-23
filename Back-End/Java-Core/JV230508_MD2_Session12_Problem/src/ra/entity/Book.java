5b3f8a8f8757e75f578988adcfee6145ca7c984e
lightofheaven278 <51086534+lightofheaven278@users.noreply.github.com>
2023-08-23 23:56:06 +0900

    Add files via upload

--- Back-End/Java-Core/JV230508_MD2_Session12_Problem/src/ra/entity/Book.java
+++ Back-End/Java-Core/JV230508_MD2_Session12_Problem/src/ra/entity/Book.java
@@ -1,2 +1,66 @@
-package ra.entity;public class Book {
+package ra.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Scanner;
+
+public class Book implements Serializable {
+    private String bookId;
+    private String bookName;
+    private Float price;
+
+
+    public Book() {
+    }
+
+    public Book(String bookId, String bookName, Float price) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+        this.price = price;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public void inputData(Scanner input, List<Book> arr) {
+        System.out.println("Please input book ID:");
+        this.bookId = input.nextLine();
+        System.out.println("Please input book name:");
+        this.bookName = input.nextLine();
+        boolean checkOut = true;
+        do {
+            try {
+                System.out.println("Please input book price:");
+                this.price = Float.parseFloat(input.nextLine());
+                checkOut = false;
+            } catch (NumberFormatException ex1) {
+                System.err.println("The inputted price is not a number. Please try again!");
+            }
+        } while (checkOut);
+    }
+
+    public void displayData() {
+        System.out.printf("%-15s%-15s%-15.1f\n", this.bookId, this.bookName, this.price);
+    }
 }
--- Back-End/Java-Core/JV230508_MD2_Session12_Problem/src/ra/run/Main.java
+++ Back-End/Java-Core/JV230508_MD2_Session12_Problem/src/ra/run/Main.java
@@ -1,2 +1,113 @@
-package ra.run;public class Main {
+package ra.run;
+
+import ra.entity.Book;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    static List<Book> bookList = new ArrayList<>();
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        do {
+            System.out.println("--------------MENU-------------");
+            System.out.println("1. Input book's info");
+            System.out.println("2. Write info to file demo.txt");
+            System.out.println("3. Read file demo.txt and print book whose price is from 10000 to 200000");
+            System.out.println("4. Exit");
+            System.out.println("Please input your choice:");
+            int choice = Integer.parseInt(input.nextLine());
+            switch (choice) {
+                case 1 -> {
+                    Book newBook = new Book();
+                    newBook.inputData(input, bookList);
+                    bookList.add(newBook);
+                }
+                case 2 -> {
+                    Main.writeDataToFile(bookList);
+                    System.out.printf("%-15s%-15s%-15s\n", "Book ID", "Book Name", "Price");
+                    for (Book book : bookList) {
+                        book.displayData();
+                    }
+                    System.out.println("The inputted data has already been written to file!");
+                }
+                case 3 -> {
+                    List<Book> listBookRead = Main.readDataFromFile();
+                    System.out.printf("%-15s%-15s%-15s\n", "Book ID", "Book Name", "Price");
+                    for (Book book : listBookRead) {
+                        if (book.getPrice() > 10000 && book.getPrice() < 20000) {
+                            book.displayData();
+                        }
+                    }
+                }
+                case 4 -> System.exit(0);
+                default -> System.err.println("The inputted choice is out of scope!");
+            }
+        } while (true);
+    }
+
+    public static void writeDataToFile(List<Book> arr) {
+        File newFile = new File("demo.txt");
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        try {
+            fos = new FileOutputStream(newFile);
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(arr);
+        } catch (FileNotFoundException ex1) {
+            System.err.println("File is not exist!");
+        } catch (IOException ex2) {
+            System.err.println("Err appears while writing data!");
+        } catch (Exception ex) {
+            System.err.println("There is an err. Please try again!");
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException ex1) {
+                System.err.println("Err appears while closing stream!");
+            } catch (Exception ex) {
+                System.err.println("Err appears while closing any stream!");
+            }
+        }
+    }
+
+    public static List<Book> readDataFromFile() {
+        File file = new File("demo.txt");
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        List<Book> listBookRead = null;
+        try {
+            fis = new FileInputStream(file);
+            ois = new ObjectInputStream(fis);
+            listBookRead = (List<Book>) ois.readObject();
+        } catch (FileNotFoundException ex1) {
+            System.err.println("The file is not exist!");
+        } catch (IOException ex2) {
+            System.err.println("Err appears while reading file!");
+        } catch (Exception ex) {
+            System.err.println("There are some err appear while reading file!");
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (IOException ex1) {
+                System.err.println("Err appears while closing stream");
+            } catch (Exception ex) {
+                System.err.println("There are some err appearing while closing stream");
+            }
+        }
+        return listBookRead;
+    }
 }
